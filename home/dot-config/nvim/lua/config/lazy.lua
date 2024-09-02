b8d0be4a2da0465022b2111f3a987b86e0a4497f
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2024-09-03 01:31:31 +0300

    fixed missing formatter fo markdown files

--- home/dot-config/nvim/lua/config/lazy.lua
+++ home/dot-config/nvim/lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.coding.copilot-chat" },
     { import = "lazyvim.plugins.extras.dap.core" },
