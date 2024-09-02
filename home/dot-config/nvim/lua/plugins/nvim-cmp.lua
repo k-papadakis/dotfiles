035f22d476f401d753270b6720757ac5a23e79a1
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2024-09-03 01:24:03 +0300

    re-enabled automatic completions

--- home/dot-config/nvim/lua/plugins/nvim-cmp.lua
+++ home/dot-config/nvim/lua/plugins/nvim-cmp.lua
@@ -3,10 +3,10 @@ return {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       -- Prevent accepting with Enter. Usefull when `completion.autocomplete` is false.
-      -- opts.mapping["<CR>"] = nil
+      opts.mapping["<CR>"] = nil
 
       -- Autocomplete on demand (use <c-N> or <c-P> to trigger).
-      opts.completion.autocomplete = false
+      -- opts.completion.autocomplete = false
     end,
   },
 }
