630cef318d534d998f0740d1c16a28f1961205c7
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2024-09-03 04:29:09 +0300

    Included and modified the entire nvim-cmp config to ease changes

--- home/dot-config/nvim/lua/plugins/nvim-cmp.lua
+++ home/dot-config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,14 +1,76 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      -- Prevent accepting with Enter.
-      -- Usefull when `completion.autocomplete` is false.
-      -- Use <C-y> instead.
-      opts.mapping["<CR>"] = nil
+  "hrsh7th/nvim-cmp",
+  opts = function()
+    vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+    local cmp = require("cmp")
+    local defaults = require("cmp.config.default")()
+    local auto_select = false
+    local text_completions = false
+    local disable_autocomplete = false
+    return {
+      auto_brackets = {}, -- configure any filetype to auto add brackets
+      completion = {
+        completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
+        autocomplete = disable_autocomplete and { false } or nil,
+      },
+      preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
+      mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = LazyVim.cmp.confirm({ select = auto_select }),
+        ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
+        ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-CR>"] = function(fallback)
+          cmp.abort()
+          fallback()
+        end,
+      }),
+      sources = cmp.config.sources({
+        {
+          name = "nvim_lsp",
+          entry_filter = function(entry, ctx)
+            return text_completions or require("cmp.types").lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
+          end,
+        },
+        {
+          name = "path",
+        },
+      }, {
+        {
+          name = "buffer",
+          entry_filter = function(entry, ctx)
+            return text_completions or require("cmp.types").lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
+          end,
+        },
+      }),
+      formatting = {
+        format = function(entry, item)
+          local icons = LazyVim.config.icons.kinds
+          if icons[item.kind] then
+            item.kind = icons[item.kind] .. item.kind
+          end
 
-      -- Autocomplete on demand. Use <C-Space> trigger.
-      -- opts.completion.autocomplete = false
-    end,
-  },
+          local widths = {
+            abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+            menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+          }
+
+          for key, width in pairs(widths) do
+            if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+              item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
+            end
+          end
+
+          return item
+        end,
+      },
+      experimental = {
+        ghost_text = {
+          hl_group = "CmpGhostText",
+        },
+      },
+      sorting = defaults.sorting,
+    }
+  end,
 }
