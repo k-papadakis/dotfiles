05e0cc04dddbdd7f9f771bdaf6076be83ca0d38d
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2024-09-07 21:37:06 +0300

    Reset H and L to vim defaults
    LazyVim was overriding them with prev buffer and next buffer.

--- home/dot-config/nvim/lua/config/keymaps.lua
+++ home/dot-config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
 vim.keymap.set("i", "jk", "<ESC>", { silent = true })
+
+-- Remove LazyVim's Prev/Next Buffer with H and L,
+-- because they override vim defaults.
+-- Just use [b and ]b.
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
