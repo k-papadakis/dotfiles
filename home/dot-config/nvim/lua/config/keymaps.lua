c6a303bb31c4c1b57dcc14aae700e5b124ef85a6
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2024-09-16 14:03:32 +0300

    Revert <S-h> and <S-l> to the original LazyVim behavior.
    Jumping to High/Low/Mid is almost never used,
    since there are better ways to jump around.
    Buffer navigation is performed all the time,
    and continuously pressing ]b and [b is cumbersome.

--- home/dot-config/nvim/lua/config/keymaps.lua
+++ home/dot-config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
--- Remove LazyVim's Prev/Next Buffer with H and L,
--- because they override vim defaults.
--- Just use [b and ]b.
-vim.keymap.del("n", "<S-h>")
-vim.keymap.del("n", "<S-l>")
