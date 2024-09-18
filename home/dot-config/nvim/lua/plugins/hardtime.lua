9af4eeecebd88b379fe96d059771fc15b9bbd3fe
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2024-09-19 01:23:45 +0300

    Allowing mouse in hardtime.

--- home/dot-config/nvim/lua/plugins/hardtime.lua
+++ home/dot-config/nvim/lua/plugins/hardtime.lua
@@ -2,5 +2,7 @@ return {
   "m4xshen/hardtime.nvim",
   event = "VeryLazy",
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  opts = {},
+  opts = {
+    disable_mouse = false,
+  },
 }
