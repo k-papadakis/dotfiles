9baf7ec370b2f2c055505537611537f84e707845
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2024-09-21 16:49:44 +0300

    Changed Copilot's keymaps again.

--- home/dot-config/nvim/lua/plugins/copilot.lua
+++ home/dot-config/nvim/lua/plugins/copilot.lua
@@ -18,5 +18,12 @@ return {
       markdown = true,
       help = true,
     },
+    suggestion = {
+      keymap = {
+        accept_word = "<M-w>",
+        accept_line = "<M-l>",
+        accept = "<M-y>",
+      },
+    },
   },
 }
