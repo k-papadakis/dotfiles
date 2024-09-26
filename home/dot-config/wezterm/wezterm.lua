ee4bb2dba650f80bd71b447e1e7916c4d0d12d75
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2024-09-27 00:38:57 +0300

    Removed transparency

--- home/dot-config/wezterm/wezterm.lua
+++ home/dot-config/wezterm/wezterm.lua
@@ -7,6 +7,5 @@ config.font_size = 13
 config.line_height = 1.2
 config.send_composed_key_when_right_alt_is_pressed = false
 config.enable_tab_bar = false
-config.background = require("alien_background")
 
 return config
