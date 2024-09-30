57bfbfc10d72b8d3071e846daed6aa8f95287708
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2024-09-30 13:14:23 +0300

    Removed databricks asset bundles schema in yamls
    To use a schema just add a comment at the top of the file like so:
    `yaml-language-server: $schema=path/to/schema`

--- home/dot-config/nvim/lua/plugins/nvim-lspconfig.lua
+++ home/dot-config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -5,18 +5,7 @@ return {
       bashls = {},
       -- FIXME: gitlab_ci_ls is supposed to have more functionality, like gd
       gitlab_ci_ls = {},
-      yamlls = {
-        settings = {
-          yaml = {
-            schemas = {
-              -- Databricks Asset Bundles schema. To activate it, run
-              -- `databricks bundle schema > .bundle_config_schema.json`
-              -- in a Databricks Asset Bundles project
-              ["./.bundle_config_schema.json"] = { "databricks.yml", "databricks/**/*.{yml,yaml}" },
-            },
-          },
-        },
-      },
+      yamlls = {},
     },
   },
 }
