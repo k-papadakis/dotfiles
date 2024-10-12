ab5bbe776f8912d7c97cff52751c781b538234c1
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2024-10-12 14:48:18 +0300

    Fixed lazygit broken colors when in tmux

--- home/dot-config/zsh/aliases.zsh
+++ home/dot-config/zsh/aliases.zsh
@@ -1,15 +1,19 @@
 # ALIASES
 
-alias v="nvim"
-alias docker="podman"
-alias cd="z"
-alias ls="eza --color=auto --icons=auto"
-alias tree="eza --tree --color=auto --icons=auto"
-alias ll="eza --long --color=auto --icons=auto --git"
-alias la="eza --long --all --color=auto --icons=auto --git"
-alias lzg="lazygit"
-alias lzd="lazydocker"
-alias batman="bat --language=man --style=plain --paging=always"
+# TODO: Remove this when this issue gets fixed
+# https://github.com/jesseduffield/lazygit/issues/3668
+alias lazygit='[[ $TERM == "tmux-256color" ]] && env TERM=screen-256color lazygit || lazygit'
+
+alias v='nvim'
+alias docker='podman'
+alias cd='z'
+alias ls='eza --color=auto --icons=auto'
+alias tree='eza --tree --color=auto --icons=auto'
+alias ll='eza --long --color=auto --icons=auto --git'
+alias la='eza --long --all --color=auto --icons=auto --git'
+alias lzg='lazygit'
+alias lzd='lazydocker'
+alias batman='bat --language=man --style=plain --paging=always'
 alias -g ...='../..'
 alias -g ....='../../..'
 alias -g .....='../../../..'
