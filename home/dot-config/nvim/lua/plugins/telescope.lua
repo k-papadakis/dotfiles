403f18d85e59a8d477417cfcda7f94bd1259e7fa
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2024-10-21 15:47:22 +0300

    Telescope Terraform Docs Keymaps

--- home/dot-config/nvim/lua/plugins/telescope.lua
+++ home/dot-config/nvim/lua/plugins/telescope.lua
@@ -1,14 +1,44 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  keys = {
-    {
-      "<leader>fd",
-      function()
-        require("telescope.builtin").git_files({
-          cwd = "~/repos/personal/dotfiles",
-        })
-      end,
-      desc = "Find Dotfiles",
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<leader>fd",
+        function()
+          require("telescope.builtin").git_files({
+            cwd = "~/repos/personal/dotfiles",
+          })
+        end,
+        desc = "Find Dotfiles",
+      },
+    },
+    dependencies = {
+      {
+        "ANGkeith/telescope-terraform-doc.nvim",
+        keys = {
+          { "<leader>ott", ":Telescope terraform_doc<cr>", desc = "Terraform Docs All" },
+          { "<leader>otm", ":Telescope terraform_doc modules<cr>", desc = "Terraform Docs Modules" },
+          {
+            "<leader>ota",
+            ":Telescope terraform_doc full_name=hashicorp/azurerm<cr>",
+            desc = "Terraform Docs Azure",
+          },
+          {
+            "<leader>otd",
+            ":Telescope terraform_doc full_name=databricks/databricks<cr>",
+            desc = "Terraform Docs Databricks",
+          },
+        },
+      },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>o", group = "Open", icon = "" },
+        { "<leader>ot", group = "Terraform Docs", icon = "󱁢" },
+      },
     },
   },
 }
