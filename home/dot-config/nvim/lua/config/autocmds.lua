fcdfc5b3b448718539b16e4f495040bc7ca8457f
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2024-10-27 03:13:56 +0300

    Terraform URL opener only when LSP is attached

--- home/dot-config/nvim/lua/config/autocmds.lua
+++ home/dot-config/nvim/lua/config/autocmds.lua
@@ -19,60 +19,30 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   end,
 })
 
-local function open_terraform_url()
-  local params = vim.lsp.util.make_position_params()
-  vim.lsp.buf_request(0, "textDocument/hover", params, function(err, result, ctx, config)
-    if err then
-      print("Error: ", err)
-      return
-    end
-    if not (result and result.contents) then
-      print("No hover information available")
-      return
-    end
-    local hover_message = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
-    if #hover_message < 2 then
-      print("Incomplete hover information")
-      return
-    end
-
-    local line1 = hover_message[1]
-    local line2 = hover_message[3]
-
-    -- Extract provider, name, and version from line1
-    local resource, provider, name, version = line1:match("`[^_]*_(.+)` ([^/]+)/([^/]+) ([^ ]+)")
-    if not (resource and provider and name and version) then
-      print("Failed to parse line 1")
-      return
-    end
-
-    -- Determine the type from line2
-    local type_part = (line2 == "Data Source Type") and "data-sources"
-      or (line2 == "Resource Type") and "resources"
-      or nil
-    if not type_part then
-      print("Invalid type in line 2")
-      return
-    end
-
-    -- Construct the URL
-    local url = string.format(
-      "https://registry.terraform.io/providers/%s/%s/%s/docs/%s/%s",
-      provider,
-      name,
-      version,
-      type_part,
-      resource
-    )
-
-    -- Open the URL
-    vim.ui.open(url)
-  end)
-end
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "terraform",
+-- Open Terraform documentation URLs
+vim.api.nvim_create_autocmd("LspAttach", {
+  pattern = "*.tf",
   callback = function(event)
-    vim.keymap.set("n", "<Leader>otx", open_terraform_url, { buffer = event.buf, desc = "Open in Terraform Docs" })
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if client and client.name == "terraformls" then
+      vim.keymap.set("n", "<Leader>otx", function()
+        local params = vim.lsp.util.make_position_params()
+        vim.lsp.buf_request(0, "textDocument/hover", params, function(err, result)
+          if err or not result.contents then
+            return
+          end
+          local hover_message = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+          local type = (hover_message[3] == "Data Source Type") and "data-sources"
+            or (hover_message[3] == "Resource Type") and "resources"
+          local resource, provider, name, version = hover_message[1]:match("`[^_]*_(.+)` ([^/]+)/([^/]+) ([^ ]+)")
+          if not (type and resource and provider and name and version) then
+            return
+          end
+          local url_template = "https://registry.terraform.io/providers/%s/%s/%s/docs/%s/%s"
+          local url = string.format(url_template, provider, name, version, type, resource)
+          vim.ui.open(url)
+        end)
+      end, { buffer = event.buf, desc = "Open in Terraform Docs" })
+    end
   end,
 })
