36806f5a2087ca54bb142da3b69254f77e00a9d4
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2025-02-15 02:14:30 +0200

    Added gruvbox material

--- home/dot-config/zsh/programs.zsh
+++ home/dot-config/zsh/programs.zsh
@@ -5,8 +5,9 @@ eval "$(gh copilot alias -- zsh)"
 eval "$(direnv hook zsh)"
 eval "$(zoxide init zsh)"
 
-source "${ZDOTDIR}/themes/fzf/catppuccin-macchiato.sh"
+# source "${ZDOTDIR}/themes/fzf/catppuccin-macchiato.sh"
+source "${ZDOTDIR}/themes/fzf/gruvbox-material-dark.sh"
 source <(fzf --zsh)
 source "${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-source "${ZDOTDIR}/themes/zsh-syntax-highlighting/catppuccin-macchiato.zsh"
+# source "${ZDOTDIR}/themes/zsh-syntax-highlighting/catppuccin-macchiato.zsh"
 source "${HOMEBREW_PREFIX}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
