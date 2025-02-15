53fd41f8352df71ac6079ae3fb94fa8b22057d16
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2025-02-15 12:25:03 +0200

    Reintroducing tinted-fzf

--- home/dot-config/zsh/programs.zsh
+++ home/dot-config/zsh/programs.zsh
@@ -6,7 +6,7 @@ eval "$(direnv hook zsh)"
 eval "$(zoxide init zsh)"
 
 # source "${ZDOTDIR}/themes/fzf/catppuccin-macchiato.sh"
-source "${ZDOTDIR}/themes/fzf/gruvbox-material-dark.sh"
+source "${ZDOTDIR}/themes/fzf/tinted-fzf/sh/base16-gruvbox-material-dark-medium.sh"
 source <(fzf --zsh)
 source "${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 # source "${ZDOTDIR}/themes/zsh-syntax-highlighting/catppuccin-macchiato.zsh"
--- home/dot-config/zsh/themes/fzf/gruvbox-material-dark.sh
+++
@@ -1,5 +0,0 @@
-export FZF_DEFAULT_OPTS=" \
- --color=bg+:#32302f,bg:#292828,spinner:#89b482,hl:#7daea3 \
- --color=fg:#bdae93,header:#7daea3,info:#d8a657,pointer:#89b482 \
- --color=marker:#89b482,fg+:#ebdbb2,prompt:#d8a657,hl+:#7daea3 \
- --multi"
