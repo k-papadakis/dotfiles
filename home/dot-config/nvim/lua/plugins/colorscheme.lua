78fd4979663de6f391fa7f35cb6a6a0e8027ebbb
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2025-02-16 17:56:57 +0200

    Fix gruvbox colors

--- home/dot-config/nvim/lua/plugins/colorscheme.lua
+++ home/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -17,10 +17,21 @@ return {
     "sainnhe/gruvbox-material",
     lazy = true,
     config = function()
-      -- :h gruvbox-material
       vim.g.gruvbox_material_float_style = "dim"
       vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
       vim.g.gruvbox_material_diagnostic_line_highlight = 1
+
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        group = vim.api.nvim_create_augroup("custom_highlights_gruvboxmaterial", {}),
+        pattern = "gruvbox-material",
+        callback = function()
+          local config = vim.fn["gruvbox_material#get_configuration"]()
+          local palette =
+            vim.fn["gruvbox_material#get_palette"](config.background, config.foreground, config.colors_override)
+          local set_hl = vim.fn["gruvbox_material#highlight"]
+          set_hl("FlashMatch", palette.fg0, palette.bg4)
+        end,
+      })
     end,
   },
   {
