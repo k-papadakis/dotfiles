c6636a75e2cd50ca60572c3d4d0e85ef0b0a5200
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2025-02-23 10:59:38 +0200

    mini-hipatterns instead of nvim-colorizer

--- home/dot-config/nvim/lua/config/lazy.lua
+++ home/dot-config/nvim/lua/config/lazy.lua
@@ -40,6 +40,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.ui.smear-cursor" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "plugins" },
   },
