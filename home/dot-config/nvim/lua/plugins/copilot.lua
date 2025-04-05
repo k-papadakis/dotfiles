c463a6255e5f399fe2cede031942bedd857c1871
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2025-04-05 22:59:20 +0300

    Refactored copilot-toggle keymap

--- home/dot-config/nvim/lua/plugins/copilot.lua
+++ home/dot-config/nvim/lua/plugins/copilot.lua
@@ -6,4 +6,23 @@ return {
       yaml = true,
     },
   },
+  config = function(_, opts)
+    -- https://github.com/LazyVim/LazyVim/discussions/4232#discussioncomment-11191278
+    require("copilot").setup(opts)
+    require("snacks")
+      .toggle({
+        name = "Copilot",
+        get = function()
+          return not require("copilot.client").is_disabled()
+        end,
+        set = function(state)
+          if state then
+            require("copilot.command").enable()
+          else
+            require("copilot.command").disable()
+          end
+        end,
+      })
+      :map("<leader>at")
+  end,
 }
