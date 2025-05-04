8dff2d22ac06cac2e328369f78e7f01fb068b192
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2025-05-04 11:33:31 +0300

    feat(nvim): record [count]j and [count]k to the jumplist

--- home/dot-config/nvim/lua/config/keymaps.lua
+++ home/dot-config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("i", "jk", "<ESC>", { silent = true })
+local map = vim.keymap.set
+
+map("i", "jk", "<ESC>", { silent = true })
+
+-- Record [count]Up/Down to the jump list
+map({ "n", "x" }, "j", function()
+  return vim.v.count > 0 and "m'" .. vim.v.count .. "j" or "gj"
+end, { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "<Down>", function()
+  return vim.v.count > 0 and "m'" .. vim.v.count .. "j" or "gj"
+end, { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "k", function()
+  return vim.v.count > 0 and "m'" .. vim.v.count .. "k" or "gk"
+end, { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "<Up>", function()
+  return vim.v.count > 0 and "m'" .. vim.v.count .. "k" or "gk"
+end, { desc = "Down", expr = true, silent = true })
