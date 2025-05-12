cfec68ff51a32f87a37119674cf28095b2826876
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2025-05-12 13:00:28 +0300

    revert: "fix(nvim): temp pin mason to v1 till LazyVim updates"
    This reverts commit fff7e1275bd350eb5a1320f5aaed09c44dc69661.

--- home/dot-config/nvim/lua/plugins/mason.lua
+++ home/dot-config/nvim/lua/plugins/mason.lua
@@ -1,11 +1,4 @@
 return {
-  {
-    "mason.nvim",
-    opts = { ensure_installed = { "shellcheck", "shfmt" } },
-  },
-
-  -- TODO: unpin when LazyVim gets up to date
-  -- https://github.com/LazyVim/LazyVim/issues/6039
-  { "mason.nvim", version = "^1.0.0" },
-  { "mason-lspconfig.nvim", version = "^1.0.0" },
+  "mason.nvim",
+  opts = { ensure_installed = { "shellcheck", "shfmt" } },
 }
