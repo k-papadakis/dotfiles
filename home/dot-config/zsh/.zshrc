317c0c4df4cac104d36016eb4c337fc4f645b572
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2025-06-05 00:57:08 +0300

    refactor(zsh): move .zshenv to .zshrc

--- home/dot-config/zsh/.zshrc
+++ home/dot-config/zsh/.zshrc
@@ -2,6 +2,16 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_STATE_HOME="${HOME}/.local/state"
+
+export PATH="${PATH}:${HOME}/.local/bin:${HOME}/.cargo/bin"
+
+fpath+=${HOMEBREW_PREFIX}/share/zsh/site-functions
+fpath+=${ZDOTDIR}/.zfunc
+
 source ${ZDOTDIR}/opts.zsh
 source ${ZDOTDIR}/keybindings.zsh
 source ${ZDOTDIR}/completions.zsh
--- home/dot-config/zsh/programs.zsh
+++ home/dot-config/zsh/programs.zsh
@@ -1,3 +1,52 @@
+export HOMEBREW_BUNDLE_DUMP_NO_VSCODE=1
+export HOMEBREW_BUNDLE_INSTALL_CLEANUP=1
+
+export STARSHIP_CONFIG="${XDG_CONFIG_HOME}/starship/starship.toml"
+
+# export BAT_THEME="Catppuccin Macchiato"
+# export BAT_THEME="gruvbox-material-dark"
+export BAT_THEME="kanagawa"
+
+# The colors are messed up when using bat for manpages on linux.
+# Nvim is great for manpaging, although a bit slow.
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  export MANPAGER='nvim +Man!'
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  export MANPAGER="sh -c 'col -bx | bat --language=man --style=plain'"
+fi
+
+export LESS="-i"
+export LESSUTFCHARDEF="e000-f8ff:p,f0001-fffff:p"  # nerd font symbols
+
+# Default Editor
+if [[ -n ${SSH_CONNECTION} ]]; then
+  export VISUAL=vim
+else
+  export VISUAL=nvim
+fi
+export EDITOR="${VISUAL}"
+
+# Prevent Podman from using the OCI format
+export BUILDAH_FORMAT=docker
+
+# Source: https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/theme-and-appearance.zsh
+# Sets color variable such as $fg, $bg, $color and $reset_color
+autoload -U colors && colors
+# Default coloring for BSD-based ls
+export LSCOLORS="Gxfxcxdxbxegedabagacad"
+# Default coloring for GNU-based ls
+if [[ -z "$LS_COLORS" ]]; then
+  # Define LS_COLORS via dircolors if available. Otherwise, set a default
+  # equivalent to LSCOLORS (generated via https://geoff.greer.fm/lscolors)
+  if (( $+commands[dircolors] )); then
+    [[ -f "$HOME/.dircolors" ]] \
+      && source <(dircolors -b "$HOME/.dircolors") \
+      || source <(dircolors -b)
+  else
+    export LS_COLORS="di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
+  fi
+fi
+
 eval "$(starship init zsh)"
 eval "$(gh copilot alias -- zsh)"
 eval "$(direnv hook zsh)"
