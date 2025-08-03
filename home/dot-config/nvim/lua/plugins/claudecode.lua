dad7663357fee216856df2416299efbeaec3a659
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2025-08-03 16:12:44 +0300

    feat: add claude code

---
+++ home/dot-config/nvim/lua/plugins/claudecode.lua
@@ -0,0 +1,32 @@
+return {
+  "coder/claudecode.nvim",
+  -- HACK: The terminal buffer opened by this plugin does not respect hjkl
+  -- init = function()
+  --   vim.api.nvim_create_autocmd("TermOpen", {
+  --     callback = function()
+  --       vim.keymap.set("t", "<C-h>", "<C-h>", { buffer = true, noremap = true })
+  --     end,
+  --   })
+  -- end,
+  dependencies = { "folke/snacks.nvim" },
+  config = true,
+  keys = {
+    { "<leader>a", nil, desc = "AI/Claude Code" },
+    { "<leader>aa", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+    { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
+    { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
+    { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+    { "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
+    { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+    { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+    {
+      "<leader>as",
+      "<cmd>ClaudeCodeTreeAdd<cr>",
+      desc = "Add file",
+      ft = { "NvimTree", "neo-tree", "oil" },
+    },
+    -- Diff management
+    { "<leader>ay", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+    { "<leader>an", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+  },
+}
--- home/dot-config/nvim/lua/plugins/codecompanion.lua
+++ home/dot-config/nvim/lua/plugins/codecompanion.lua
@@ -1,6 +1,7 @@
 return {
   {
     "olimorris/codecompanion.nvim",
+    enabled = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
