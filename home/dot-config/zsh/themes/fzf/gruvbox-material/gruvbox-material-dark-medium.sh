4d44262c574733cb2f049fe7a74dfa30eaf6f7b9
Konstantinos Papadakis <k.i.papadakis@gmail.com>
2026-07-14 23:15:11 +0300

    feat(zsh): vendor fzf theme colors, drop tinted-fzf

---
+++ home/dot-config/zsh/themes/fzf/gruvbox-material/gruvbox-material-dark-medium.sh
@@ -0,0 +1,27 @@
+# Gruvbox Material fzf colors (dark background, medium contrast)
+# Derived from the sainnhe/gruvbox-material fzf.vim palette:
+# https://github.com/sainnhe/gruvbox-material/blob/master/colors/gruvbox-material.vim
+
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+  --highlight-line \
+  --info=inline-right \
+  --ansi \
+  --layout=reverse \
+  --border=none \
+  --color=bg+:#32302f \
+  --color=bg:#282828 \
+  --color=border:#928374 \
+  --color=fg:#d4be98 \
+  --color=gutter:#282828 \
+  --color=header:#928374 \
+  --color=hl+:#89b482 \
+  --color=hl:#a9b665 \
+  --color=info:#89b482 \
+  --color=marker:#d8a657 \
+  --color=pointer:#7daea3 \
+  --color=prompt:#e78a4e \
+  --color=query:#d4be98:regular \
+  --color=scrollbar:#928374 \
+  --color=separator:#928374 \
+  --color=spinner:#d8a657 \
+"
---
+++ home/dot-config/zsh/themes/fzf/kanagawa/kanagawa-wave.sh
@@ -0,0 +1,24 @@
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+  --highlight-line \
+  --info=inline-right \
+  --ansi \
+  --layout=reverse \
+  --border=sharp \
+  --style=full \
+  --color=bg+:#2d4f67 \
+  --color=bg:#1f1f28 \
+  --color=border:#54546d \
+  --color=fg:#dcd7ba \
+  --color=gutter:#1f1f28 \
+  --color=header:#c34043 \
+  --color=hl+:#e6c384 \
+  --color=hl:#c0a36e \
+  --color=info:#727169 \
+  --color=marker:#76946a \
+  --color=pointer:#7e9cd8 \
+  --color=prompt:#957fb8 \
+  --color=query:#dcd7ba:regular \
+  --color=scrollbar:#54546d \
+  --color=separator:#54546d \
+  --color=spinner:#ffa066 \
+"
